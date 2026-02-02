9144c74c4ae3aa7ce4f450fd31b4fe8e795758bb
nizalovnd <ndn1@kent.ac.uk>
2026-02-02 13:11:36 +0000

    camel to snake conveerter

--- camel.py
+++ camel.py
@@ -1,1 +1,15 @@
 # Enter your code here
+def main():
+    output_name = ""
+    input_name = input("Enter your name in camelCase:")
+    for l in input_name:
+        if l.isupper():
+            output_name += "_"+ l.casefold()
+        elif l.islower():
+            output_name += l
+        else:
+            continue
+    print("Your name in snake case is:" + output_name)
+
+if __name__ == "__main__":
+    main()
