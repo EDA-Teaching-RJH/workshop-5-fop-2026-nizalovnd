1096a3cdca2dd751dbb3b9bac9966f158446f23c
nizalovnd <ndn1@kent.ac.uk>
2026-02-02 13:59:56 +0000

    coffee machine script v4

--- coffee.py
+++ coffee.py
@@ -12,30 +12,45 @@ The system should not crash and disregard the invalid input and continue to ask 
 coffee_price = 75
 accepted_coins = [ "50p","20p","10p" ,"5p"]
 
+def get_coin():
+    coin = input("Insert a coin")
+    if coin not in accepted_coins:
+        print("Invalid coin inserted. Collect rejected coin")
+        return 0
+    coin_val = ""
+    for l in coin:
+        if l.isnumeric():
+            coin_val += l
+        else:
+            continue
+    coin_num = int(coin_val)
+    return coin_num
+
+def update_total(current, coin):
+    current += coin
+    return current
+
+def dispense_product(total_inserted):
+    if total_inserted == coffee_price:
+        print("Enjoy your coffee!")
+    elif total_inserted > coffee_price:
+        print(f"The change due is {total_inserted - coffee_price}p")
+        print("Enjoy your coffee and don't forget your change!")
+
+
+
+
 def main():
     print("The price of the cofee is 75p!")
     print("The machine only accepts 50p, 20p, 10p and 5p coins")
     inserted_money = 0
     while inserted_money < coffee_price:
         print(f"You need to insert {coffee_price - inserted_money}p more")
-        coin = input("Insert a coin")
-        if coin not in accepted_coins:
-            print("Invalid coin inserted. Collect rejected coin")
-            continue
-        coin_val = ""
-        for l in coin:
-            if l.isnumeric():
-                coin_val += l
-            else:
-                continue
-        coin_num = int(coin_val)
-        inserted_money += coin_num
+        coin_num = get_coin()
+        inserted_money = update_total(inserted_money, coin_num)
+        dispense_product(inserted_money)
         
-    if inserted_money == coffee_price:
-        print("Enjoy your coffee!")
-    elif inserted_money > coffee_price:
-        print(f"The change due is {inserted_money - coffee_price}p")
-        print("Enjoy your coffee and don't forget your change!")
+    
 
 if __name__ == "__main__":
     main()
